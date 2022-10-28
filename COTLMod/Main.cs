ec26a7ec98034779da5ef682e87d0a3f4ce56360
Scope <98496609+ScrimCreations@users.noreply.github.com>
2022-10-28 16:13:19 -0400

    Base setup

--- COTLMod/Main.cs
+++ COTLMod/Main.cs
@@ -1,15 +1,38 @@
-﻿using System;
+﻿using MelonLoader;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
+using COTLMod;
 
-
+[assembly: MelonInfo(typeof(MainMod), "COTLMod", "1", "Scrim")]
+[assembly: MelonColor(ConsoleColor.DarkRed)]
+[assembly: MelonPriority(0)]
 
 namespace COTLMod
 {
-    public class Main
+    public class MainMod : MelonMod
     {
+        public override void OnApplicationStart()
+        {
+            
+        }
 
+        public override void OnGUI()
+        {
+            
+        }
+
+        public override void OnUpdate()
+        {
+            
+        }
+
+        public override void OnApplicationQuit()
+        {
+            
+        }
     }
 }
